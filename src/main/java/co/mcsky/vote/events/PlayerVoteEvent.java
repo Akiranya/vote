ec50b17eab6b10e85b258c0d32f88d5ee25266d7
Nailm <porridgen@gmail.com>
2021-01-28 23:25:39 +0800

    Fixes .gitignore

--- src/main/java/co/mcsky/vote/events/PlayerVoteEvent.java
+++ src/main/java/co/mcsky/vote/events/PlayerVoteEvent.java
@@ -8,6 +8,7 @@ import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.player.PlayerEvent;
 
+@SuppressWarnings("NullableProblems")
 public class PlayerVoteEvent extends PlayerEvent implements Cancellable {
 
     private static final HandlerList handlers = new HandlerList();
