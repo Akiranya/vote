e680d55c301124f18fa4bba379ea343c9486e5a7
Nailm <porridgen@gmail.com>
2021-02-11 05:07:20 +0800

    Adds javadoc for interfaces

--- src/main/java/co/mcsky/vote/type/Plot.java
+++ src/main/java/co/mcsky/vote/type/Plot.java
@@ -6,18 +6,53 @@ import java.util.UUID;
 
 public interface Plot {
 
+    /**
+     * Teleports the player to this plot.
+     *
+     * @param player the player to be teleported
+     */
     void teleport(Player player);
 
+    /**
+     * Gets the UUID of this plot owner.
+     *
+     * @return the UUID of this plot owner
+     */
     UUID getOwner();
 
+    /**
+     * Gets the name of this plot owner.
+     *
+     * @return the name of this plot owner
+     */
     String getOwnerName();
 
+    /**
+     * Check if this plot has owner.
+     *
+     * @return true if this plot has owner
+     */
     boolean hasOwner();
 
+    /**
+     * Check if this plot is marked as done.
+     *
+     * @return true if this plot is marked as done
+     */
     boolean isDone();
 
+    /**
+     * Gets the name of the world where this plot is in.
+     *
+     * @return the name of the world where this plot is in
+     */
     String getWorldName();
 
+    /**
+     * Gets the ID of this plot. The ID should be distinct to different plots.
+     *
+     * @return the ID of this plot
+     */
     String getId();
 
 }
--- src/main/java/co/mcsky/vote/type/PlotAPI.java
+++ src/main/java/co/mcsky/vote/type/PlotAPI.java
@@ -6,10 +6,27 @@ import java.util.Set;
 
 public interface PlotAPI<T> {
 
+    /**
+     * Get all plots in this plot system.
+     *
+     * @return all plots in this plot system.
+     */
     Set<Plot> getAllPlots();
 
+    /**
+     * Check if the world is a plot world. A world is a plot world if and only if the world has at least one plot which
+     * has owner.
+     *
+     * @param world the world to check
+     * @return true if the world is a plot world
+     */
     boolean isPlotWorld(World world);
 
+    /**
+     * Gets the backed internal API.
+     *
+     * @return the backed internal API.
+     */
     T internal();
 
 }
