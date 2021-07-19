c0cc3c5080063c7305e82e2e09594987fb75031a
Akiranya <porridgen@gmail.com>
2021-07-19 18:22:52 +0800

    Revert "Adds cache for game stats lookup"
    This reverts commit fb7964a3

--- src/main/java/co/mcsky/vote/type/GameStatsImpl.java
+++ src/main/java/co/mcsky/vote/type/GameStatsImpl.java
@@ -1,13 +1,7 @@
 package co.mcsky.vote.type;
 
-import me.lucko.helper.cache.Expiring;
-
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
-import java.util.concurrent.TimeUnit;
-import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -17,72 +11,48 @@ import java.util.stream.Stream;
 public class GameStatsImpl implements GameStats {
 
     // The instance of Votes from which the statistics is generated
-    private final Game game;
+    private final Game votes;
 
-    // All caches
-    private final Map<UUID, Expiring<Stream<Work>>> cachedMissedWorks;
-    private Expiring<Stream<UUID>> cachedRaters;
-    private Expiring<Set<Work>> cachedRedWorks;
-    private Expiring<Set<Work>> cachedGreenWorks;
-
-    private static <T> Expiring<T> wrapExpiring(Supplier<T> supp) {
-        // define the settings of cache
-        return Expiring.suppliedBy(supp, 60, TimeUnit.SECONDS);
-    }
-
-    public GameStatsImpl(Game game) {
-        this.game = game;
-        this.cachedMissedWorks = new HashMap<>();
+    public GameStatsImpl(Game votes) {
+        this.votes = votes;
     }
 
     @Override
-    public Stream<Work> missedWorks(UUID rater) {
-        if (!cachedMissedWorks.containsKey(rater)) {
-            // if not cached, initialize the cache
-            Supplier<Stream<Work>> supp = () -> game.getWorkAll().stream().filter(work -> work.invoted(rater));
-            cachedMissedWorks.put(rater, wrapExpiring(supp));
-        }
-        return cachedMissedWorks.get(rater).get();
+    public Stream<Work> missed(UUID rater) {
+        return votes.getWorkAll().stream().filter(work -> work.invoted(rater));
     }
 
     @Override
     public boolean valid(UUID rater) {
-        return missedWorks(rater).noneMatch(Work::isDone);
+        return missed(rater).noneMatch(Work::isDone);
     }
 
     @Override
     public boolean invalid(UUID rater) {
-        return missedWorks(rater).anyMatch(Work::isDone);
+        return missed(rater).anyMatch(Work::isDone);
     }
 
     @Override
-    public Stream<UUID> raters() {
-        if (cachedRaters == null) {
-            // if not cached, initialize the cache
-            Supplier<Stream<UUID>> supp = () -> game.getWorkAll().stream()
-                    .flatMap(w -> w.getVotes().stream())
-                    .map(Vote::getRater)
-                    .distinct();
-            cachedRaters = wrapExpiring(supp);
-        }
-        return cachedRaters.get();
+    public Stream<UUID> rawRaters() {
+        return votes.getWorkAll().stream()
+                .flatMap(w -> w.getVotes().stream())
+                .map(Vote::getRater)
+                .distinct();
     }
 
     @Override
     public Set<UUID> validRaters() {
-        return raters().filter(this::valid)
-                .collect(Collectors.toSet());
+        return rawRaters().filter(this::valid).collect(Collectors.toSet());
     }
 
     @Override
     public Set<UUID> invalidRaters() {
-        return raters().filter(this::invalid)
-                .collect(Collectors.toSet());
+        return rawRaters().filter(this::invalid).collect(Collectors.toSet());
     }
 
     @Override
     public Stream<Vote> validVotes(UUID work) {
-        return game.getWork(work)
+        return votes.getWork(work)
                 .map(Work::getVotes)
                 .orElse(Set.of()).stream()
                 .filter(vote -> valid(vote.getRater()));
@@ -90,42 +60,28 @@ public class GameStatsImpl implements GameStats {
 
     @Override
     public Set<Vote> redVotes(UUID work) {
-        return validVotes(work)
-                .filter(Vote::isAbsent)
-                .collect(Collectors.toSet());
+        return validVotes(work).filter(Vote::isAbsent).collect(Collectors.toSet());
     }
 
     @Override
     public Set<Vote> greenVotes(UUID work) {
-        return validVotes(work)
-                .filter(Vote::isPresent)
-                .collect(Collectors.toSet());
-    }
-
-    @Override
-    public Set<Work> redWorks(UUID rater) {
-        if (cachedRedWorks == null) {
-            // if not cached
-            Supplier<Set<Work>> supp = () -> game.getWorkAll().stream()
-                    .filter(work -> work.voted(rater))
-                    .filter(work -> work.absent(rater))
-                    .collect(Collectors.toUnmodifiableSet());
-            cachedRedWorks = wrapExpiring(supp);
-        }
-        return cachedRedWorks.get();
+        return validVotes(work).filter(Vote::isPresent).collect(Collectors.toSet());
     }
 
     @Override
     public Set<Work> greenWorks(UUID rater) {
-        if (cachedGreenWorks == null) {
-            // if not cached, initialize the cache
-            Supplier<Set<Work>> supp = () -> game.getWorkAll().stream()
-                    .filter(work -> work.voted(rater))
-                    .filter(work -> work.present(rater))
-                    .collect(Collectors.toUnmodifiableSet());
-            cachedGreenWorks = wrapExpiring(supp);
-        }
-        return cachedGreenWorks.get();
+        return votes.getWorkAll().stream()
+                .filter(work -> work.voted(rater))
+                .filter(work -> work.present(rater))
+                .collect(Collectors.toUnmodifiableSet());
+    }
+
+    @Override
+    public Set<Work> redWorks(UUID rater) {
+        return votes.getWorkAll().stream()
+                .filter(work -> work.voted(rater))
+                .filter(work -> work.absent(rater))
+                .collect(Collectors.toUnmodifiableSet());
     }
 
 }
